1bc613dfceb8caa0ee241dbbbed17b5f9840d50d
Исаева Мария <isaevam2015@gmail.com>
2019-10-02 01:12:05 +0400

    поправлены координаты рисования

--- Draw.py
+++ Draw.py
@@ -7,26 +7,23 @@ screen = pygame.display.set_mode(size)
 def draw():
     rect_color = pygame.Color('brown')
     rect_width = 0
-    rect_point = [[50,20], [5, 250]]
+    rect_point = [[50, 20], [5, 250]]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
     rect_color = pygame.Color('white')
     rect_width = 0
     rect_point = [[50, 30], [200, 50]]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
-
     rect_color = pygame.Color('blue')
     rect_width = 0
-    rect_point = [[50, 70], [200, 50]]
+    rect_point = [[50, 80], [200, 50]]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
     rect_color = pygame.Color('red')
     rect_width = 0
-    rect_point = [[50,110], [200, 50]]
+    rect_point = [[50,130], [200, 50]]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
-
 
 
 draw()
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
-
 pygame.quit()
